e8f4af8dabf8c7677739ca102509ddcb5fba7337
Jim O'Brien <jimobrien@firmstep.com>
2013-04-29 01:34:47 +0100

    Remove console.logs from log UI

--- src/js/flingr.log.js
+++ src/js/flingr.log.js
@@ -7,7 +7,6 @@ window.flingr = window.flingr || {};
 window.flingr.log = (function(log, $, _, undefined) {
 
 	log = function(element, host, renderer) {
-		console.log('Initialising log');
 		this.host = host;
 		this.context = {};
 		this.$elem = $(element);
@@ -42,12 +41,8 @@ window.flingr.log = (function(log, $, _, undefined) {
 				}
 			};
 
-		console.log('Registered event types,', eventTypes);
-
 		_.each(eventTypes, function(makeContext, eventType) {
-			console.info('Listening for', eventType);
 			_this.host.on(eventType, function(params) {
-				console.info('Making context for', eventType, params);
 				_this.renderRow(makeContext(params));
 			});
 		});
@@ -57,7 +52,6 @@ window.flingr.log = (function(log, $, _, undefined) {
 
 	log.prototype.renderRow = function(context) {
 		$('.empty-table-notice', this.$body).hide();
-		console.log('Rendering log row', context);
 		return this.render('console.output.row', context, this.$body, false);
 	};
 
